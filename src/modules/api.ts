85eda3d9222c848a77b742abb7bfcc4e26de2086
Seogun95 <ksj083895@gmail.com>
2023-06-07 00:59:55 +0900

    feat: api.ts에 함수 파일화

---
+++ src/modules/api.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export const numberFormat = (num: number): string => {
+  return new Intl.NumberFormat().format(num);
+};
+
+export const fetchCoins = async () => {
+  return await axios
+    .get('https://api.coinpaprika.com/v1/coins')
+    .then(res => res.data);
+};
+
+const BASE_COIN_URL = 'https://api.coinpaprika.com/v1';
+
+export const fetchDataInfo = async (coinId: string) => {
+  return await axios
+    .get(`${BASE_COIN_URL}/coins/${coinId}`)
+    .then(res => res.data);
+};
+
+export const fetchDataTickers = async (coinId: string) => {
+  return await axios
+    .get(`${BASE_COIN_URL}/tickers/${coinId}`)
+    .then(res => res.data);
+};
