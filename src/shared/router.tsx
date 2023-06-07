c8091394ba75016d0b09d010280b075c602e1854
Seogun95 <ksj083895@gmail.com>
2023-06-07 11:30:50 +0900

    chore: gh-pages 배포

--- src/shared/router.tsx
+++ src/shared/router.tsx
@@ -6,41 +6,46 @@ import App from 'App';
 import { Chart, Coin, Coins, Price } from 'pages';
 import { Layout, NotFound } from 'shared/Layout';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      errorElement: (
+        <Suspense fallback={null}>
+          <NotFound />
+        </Suspense>
+      ),
+      children: [
+        {
+          element: <Layout />,
+          children: [
+            {
+              index: true,
+              element: <Coins />,
+            },
+            {
+              path: ':coinId',
+              element: <Coin />,
+              children: [
+                {
+                  path: 'chart',
+                  element: <Chart />,
+                },
+                {
+                  path: 'price',
+                  element: <Price />,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <App />,
-    errorElement: (
-      <Suspense fallback={null}>
-        <NotFound />
-      </Suspense>
-    ),
-    children: [
-      {
-        element: <Layout />,
-        children: [
-          {
-            index: true,
-            element: <Coins />,
-          },
-          {
-            path: ':coinId',
-            element: <Coin />,
-            children: [
-              {
-                path: 'chart',
-                element: <Chart />,
-              },
-              {
-                path: 'price',
-                element: <Price />,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-]);
+    basename: process.env.PUBLIC_URL,
+  }
+);
 
 export default router;
