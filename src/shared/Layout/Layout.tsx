7242affddbde337a2d89a72b4748bba32d9b4e8a
Seogun95 <ksj083895@gmail.com>
2023-06-08 16:49:37 +0900

    style: 사용하지 않는 코드 삭제

--- src/shared/Layout/Layout.tsx
+++ src/shared/Layout/Layout.tsx
@@ -4,7 +4,6 @@ import { Outlet } from 'react-router-dom';
 
 import Header from './Header';
 import { DarkModeBtn } from 'components/DarkModeBtn';
-import { media } from 'styles';
 
 export function Layout() {
   return (
